3977365cf01eca6db02906b7c19a4e3839aa05f0
Isaiah <daezyworld@gmail.com>
2018-04-13 14:30:17 +0100

    testing app purposes

---
+++ index.php
@@ -0,0 +1,52 @@
+ $ git init
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>HNG</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+    
+<body>
+
+<div class="container">
+<!--   Menu row starts here-->
+    <div class="row">
+        <div class="col "><strong>HNG</strong></div>
+        <div class="col-4">
+            <ul class="list-inline">
+              <li class="list-inline-item">Home</li>
+              <li class="list-inline-item">About</li>
+              <li class="list-inline-item">Blog</li>
+              <li class="list-inline-item">Contact Us</li>
+            </ul>
+        </div>
+    </div>
+<!-- Menu row ends here-->
+<!--Content row starts here-->
+<div class="row">
+    <h3 class="text-cn" ><?php 
+       $date = date('Y-m-d H:i:s');
+ echo $date;
+      ?></h3>
+</div>
+</div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
